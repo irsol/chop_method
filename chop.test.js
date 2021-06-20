7dd79a420ba30a68afa5f8eee36bea859502f245
irsol <irinywka@gmail.com>
2021-06-20 12:45:03 +0200

    Add test with duplicte keys in the template

--- chop.test.js
+++ chop.test.js
@@ -1,7 +1,7 @@
 import { expect } from '@jest/globals';
 import { chop } from './chop.js';
 
-test('Feels all templates correctly', () => {
+test('Fills all templates correctly', () => {
     const myTemplate = 'I like to eat {{bread}} and {{cheese}}';
     const dataTest = {
         bread: 'baguette',
@@ -10,4 +10,15 @@ test('Feels all templates correctly', () => {
 
     const expected = 'I like to eat baguette and brie cheese';
     expect(chop(myTemplate, dataTest)).toBe(expected)
-})
+});
+
+test('Fills duplicate templates', () => {
+    const myTemplate = 'I like to eat {{bread}} and {{cheese}} and {{bread}}';
+    const dataTest = {
+        bread: 'baguette',
+        cheese: 'brie cheese',
+    };
+
+    const expected = 'I like to eat baguette and brie cheese and baguette';
+    expect(chop(myTemplate, dataTest)).toBe(expected)
+});
