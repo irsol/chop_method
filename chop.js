38371eae6ba0309f307d3b1dfe92f64a0d9cb429
irsol <irinywka@gmail.com>
2021-06-20 14:20:05 +0200

    Refactor func

--- chop.js
+++ chop.js
@@ -2,22 +2,22 @@ export const chop = (template, data) => {
 
     let chopped = template
 
-    if (typeof data === 'object' && typeof template === 'string') {
-
-        for (const [key, value] of Object.entries(data)) {
-
-            if (isValidType(key) && isValidType(value)) {
-
-                chopped = chopped.replaceAll(`{{${key}}}`, value)
-            }
-            else {
-                const errorMessage = 'Dataset should only contains strings or numbers!'
-                console.log(errorMessage)
-            }
-        }
+    if (typeof data !== 'object' || typeof template !== 'string') {
+        const errorMessage = 'Template must be a string and data must be an object type!'
+        console.log(errorMessage)
+        return chopped
     }
-    else {
-        console.log('Template must be a string and data must be an object type!')
+
+    for (const [key, value] of Object.entries(data)) {
+
+        if (isValidType(key) && isValidType(value)) {
+
+            chopped = chopped.replaceAll(`{{${key}}}`, value)
+        }
+        else {
+            const errorMessage = 'Dataset should only contains strings or numbers!'
+            console.log(errorMessage)
+        }
     }
     return chopped
 }
